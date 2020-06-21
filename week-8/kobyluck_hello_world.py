237bab846b68a4458f99b160848a7cfcb360c943
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-06-21 18:34:08 -0400

    Adding Exercise 8.2

---
+++ week-8/kobyluck_hello_world.py
@@ -0,0 +1,11 @@
+# /*
+# ;============================================
+# ; Title: Exercise 8.2
+# ; Author: Professor Krasso
+# ; Date: 16 June 2020
+# ; Modified By: Jonathan Kobyluck
+# ; Description: Anaconda
+# ;===========================================
+# */
+
+print('Hello World')
