a441fa6e09cd4bd3c1e4a94d2368fe26adfdbb9a
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-06-27 19:07:33 -0400

    Adding Exercise 9.3

---
+++ week-9/kobyluck-user-update.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import pprint
+import datetime
+
+client = MongoClient('localhost', 27017)
+db = client.web335
+
+db.users.update_one(
+    {"employee_id": "0000003"},
+    {
+        "$set": {
+            "email": "jkobyluck@my365.bellevue.edu"
+        }
+    }
+)
+
+pprint.pprint(db.users.find_one({"employee_id": "0000003"}))
